78c565f0077257a928646b8d51bfab66a031b045
bharathiraja-developer <bharathiraja798@gmail.com>
2023-10-14 10:48:42 +0530

    Creating a card

---
+++ src/components/card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/card.css";
+
+function Card() {
+  return (
+    <div class="card">
+      <p>free</p>
+      <p>price</p>
+    </div>
+  );
+}
+
+export default Card;
